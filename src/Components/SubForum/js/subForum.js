072c11aba2b9f07f02bba4414ff2da649d48ab96
Rodrigo Casale <popobasura3@hotmail.com>
2022-11-13 18:31:57 -0600

    create post

--- src/Components/SubForum/js/subForum.js
+++ src/Components/SubForum/js/subForum.js
@@ -1,42 +1,37 @@
-import React,{Component} from "react";
+import React, { Component } from "react";
 import { Col, Container, Row, Button } from "react-bootstrap";
+import { Link } from "react-router-dom";
 
 import SubForumBar from "./subForumBar";
-import "../css/subForum.css"
+import "../css/subForum.css";
 
-export default class SubForum extends React.Component{
-    render(){
-        return(
-            <Container fluid
-                id = "subForumContainer">
-                <Row>
-                    <Col>
-                        <div id="subForumButtonDiv">
-                            <Button variant = "dark"> 
-                                Create Post
-                            </Button>
-                        </div>
-                    </Col>
-                </Row>
-                <Row id = "subForumSubForumRow">
-                    <div align="center" id = "subForumPosts">
-                        <SubForumBar 
-                            postTitle="New pokemon on the way! Check it out!"/>
-                        
-                        <SubForumBar 
-                            postTitle="New pokemon on the way! Check it out!"/>
-                        
-                        <SubForumBar 
-                            postTitle="New pokemon on the way! Check it out!"/>
-                        
-                        <SubForumBar 
-                            postTitle="New pokemon on the way! Check it out!"/>
-                        
-                        <SubForumBar 
-                            postTitle="New pokemon on the way! Check it out!"/>
-                    </div>
-                </Row>
-            </Container>
-        )
-    }
+function SubForum () {
+    return (
+      <Container fluid id="subForumContainer">
+        <Row>
+          <Col>
+            <div id="subForumButtonDiv">
+              <Link to="/createPost">
+                <Button variant="dark"> Create Post</Button>
+              </Link>
+            </div>
+          </Col>
+        </Row>
+        <Row id="subForumSubForumRow">
+          <div align="center" id="subForumPosts">
+            <SubForumBar postTitle="New pokemon on the way! Check it out!" />
+
+            <SubForumBar postTitle="New pokemon on the way! Check it out!" />
+
+            <SubForumBar postTitle="New pokemon on the way! Check it out!" />
+
+            <SubForumBar postTitle="New pokemon on the way! Check it out!" />
+
+            <SubForumBar postTitle="New pokemon on the way! Check it out!" />
+          </div>
+        </Row>
+      </Container>
+    );
 }
+
+export default SubForum;
