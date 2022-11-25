549efb4ffa30ceb6e6598e9e1542f02954d8f9e2
Rodrigo Casale <popobasura3@hotmail.com>
2022-11-25 10:27:20 -0600

    connected with the event dashboard

--- src/js/db/authFunctions.js
+++ src/js/db/authFunctions.js
@@ -10,6 +10,28 @@ import { auth } from "../../firebaseConfig";
 
 import { CreateDocument, CreateDocumentWithId } from "./dbFunctions";
 
+function axiosPost(data) {
+  const config = {
+    method: "POST",
+    headers: {
+      "Access-Control-Allow-Origin": "http://localhost:8080/api/blog",
+      "Access-Control-Allow-Headers":
+        "POST Authorization, Origin, Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers, Access-Control-Allow-Origin",
+      "Content-Type": "application/json",
+      Accept: "application/json",
+    },
+    body: JSON.stringify(data),
+  };
+
+  fetch("http://localhost:8080/api/blog", config)
+    .then((res) => {
+      console.log(res);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+}
+
 export async function CreateUser(email, userName, password) {
   try {
     const user = await createUserWithEmailAndPassword(auth, email, password);
@@ -22,8 +44,22 @@ export async function CreateUser(email, userName, password) {
       { email: email, isMod: false, user: userName },
       user.user.uid
     );
+    axiosPost({
+      eventId: Date.now(),
+      eventName: "User Created",
+      userId: Date.now(),
+      userName: "null",
+      eventDescription: "User " + user.user.uid + " created",
+    });
     return user;
   } catch {
+    axiosPost({
+      eventId: Date.now(),
+      eventName: "Error at Document Create",
+      userId: 0,
+      userName: "null",
+      eventDescription: "User failed to create",
+    });
     console.log("Error Creating User ");
   }
 }
@@ -33,8 +69,22 @@ export async function SignIn(email, password) {
     console.log("User: ", user);
     sessionStorage.setItem("Auth Token", user._tokenResponse.refreshToken);
     sessionStorage.setItem("uid", user.user.uid);
+    axiosPost({
+      eventId: 101,
+      eventName: "User Signed In",
+      userId: Date.now() % 1000000,
+      userName: "null",
+      eventDescription: "User " + user.user.uid + " signed in",
+    });
     return user;
   } catch {
+    axiosPost({
+      eventId: 100,
+      eventName: "User Failed Signed In",
+      userId: Date.now() % 1000000,
+      userName: "null",
+      eventDescription: "User failed to sign in",
+    });
     console.log("Error Creating User ");
   }
 }
@@ -45,8 +95,22 @@ export async function SignOut() {
     console.log("User: ", user);
     sessionStorage.removeItem("Auth Token");
     sessionStorage.removeItem("uid");
+    axiosPost({
+      eventId: 201,
+      eventName: "User Signed Out",
+      userId: Date.now() % 1000000,
+      userName: "null",
+      eventDescription: "User " + user.user.uid + " signed out",
+    });
     return user;
   } catch {
+    axiosPost({
+      eventId: 200,
+      eventName: "User Failed Signed Out",
+      userId: Date.now() % 1000000,
+      userName: "null",
+      eventDescription: "User failed  tosigned out",
+    });
     console.log("Error Creating User ");
   }
 }
--- src/js/db/dbFunctions.js
+++ src/js/db/dbFunctions.js
@@ -10,15 +10,51 @@ import {
   setDoc,
   doc,
 } from "firebase/firestore";
-import { json } from "react-router-dom";
+import axios from "axios";
 
 import { db } from "../../firebaseConfig";
+
+function axiosPost(data) {
+  const config = {
+    method: "POST",
+    headers: {
+      "Access-Control-Allow-Origin": "http://localhost:8080/api/blog",
+      "Access-Control-Allow-Headers":
+        "POST Authorization, Origin, Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers, Access-Control-Allow-Origin",
+      "Content-Type": "application/json",
+      Accept: "application/json",
+    },
+    body: JSON.stringify(data),
+  };
+
+  fetch("http://localhost:8080/api/blog", config)
+    .then((res) => {
+      console.log(res);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+}
 
 export async function CreateDocument(targetCollection, docData) {
   try {
     const docRef = await addDoc(collection(db, targetCollection), docData);
+    axiosPost({
+      eventId: 301,
+      eventName: "Document Create",
+      userId: Date.now() % 1000000,
+      userName: "null",
+      eventDescription: "Document created in " + targetCollection,
+    });
     console.log("DOCUMENT: ", docRef.id);
   } catch {
+    axiosPost({
+      eventId: 300,
+      eventName: "Error at Document Create",
+      userId: Date.now() % 1000000,
+      userName: "null",
+      eventDescription: "Document failed to create in " + targetCollection,
+    });
     console.log("Eror Adding " + targetCollection);
   }
 }
@@ -26,8 +62,22 @@ export async function CreateDocument(targetCollection, docData) {
 export async function CreateDocumentWithId(targetCollection, docData, id) {
   try {
     const docRef = await setDoc(doc(db, targetCollection, id), docData);
-    console.log("DOCUMENT: ", docRef.id);
+    axiosPost({
+      eventId: 401,
+      eventName: "Document Create",
+      userId: Date.now() % 1000000,
+      userName: "null",
+      eventDescription:
+        "Document created in " + targetCollection + " with id " + id,
+    });
   } catch {
+    axiosPost({
+      eventId: 400,
+      eventName: "Error at Document Create",
+      userId: Date.now() % 1000000,
+      userName: "null",
+      eventDescription: "Document failed to create in " + targetCollection,
+    });
     console.log("Eror Adding " + targetCollection);
   }
 }
@@ -42,8 +92,22 @@ export async function ReadDocument(targetCollection) {
       }));
       result = newData;
     });
+    axiosPost({
+      eventId: 501,
+      eventName: "Read Document",
+      userId: Date.now() % 1000000,
+      userName: "null",
+      eventDescription: "Document read in " + targetCollection,
+    });
     return result;
   } catch {
+    axiosPost({
+      eventId: 500,
+      eventName: "Error at Document Create",
+      userId: Date.now() % 1000000,
+      userName: "null",
+      eventDescription: "Document failed to create in " + targetCollection,
+    });
     return null;
   }
 }
@@ -57,7 +121,21 @@ export async function UpdateDocument(
     const docRef = await doc(db, targetCollection + "/" + targetDocument);
     const updatedRef = await updateDoc(docRef, updatedData);
     console.log("Updated Document: ", docRef.id);
+    axiosPost({
+      eventId: 601,
+      eventName: "Update Document",
+      userId: Date.now() % 1000000,
+      userName: "null",
+      eventDescription: "Document updated in " + targetCollection,
+    });
   } catch {
+    axiosPost({
+      eventId: 600,
+      eventName: "Error at Document Update",
+      userId: Date.now() % 1000000,
+      userName: "null",
+      eventDescription: "Document failed to update in " + targetCollection,
+    });
     console.log("Eror updating " + targetDocument);
   }
 }
@@ -66,8 +144,22 @@ export async function DeleteDocument(targetCollection, targetDocument) {
   try {
     const docRef = await doc(db, targetCollection + "/" + targetDocument);
     await deleteDoc(docRef);
+    axiosPost({
+      eventId: 701,
+      eventName: "Delete Document",
+      userId: Date.now() % 1000000,
+      userName: "null",
+      eventDescription: "Document deleted in " + targetCollection,
+    });
     console.log("Deleted Document with ID ", targetDocument);
   } catch {
+    axiosPost({
+      eventId: 700,
+      eventName: "Error at Document Delete",
+      userId: Date.now() % 1000000,
+      userName: "null",
+      eventDescription: "Document delete to update in " + targetCollection,
+    });
     console.log("Eror deleting " + targetDocument);
   }
 }
